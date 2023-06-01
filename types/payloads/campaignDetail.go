efdef65d1515910cb00820473a0cdbfd3ccfe84c
bbbung <kanyapat.mekv@kmutt.ac.th>
2023-06-02 01:12:19 +0700

    [create] payload campaignDetail

---
+++ types/payloads/campaignDetail.go
@@ -0,0 +1,18 @@
+package payloads
+
+import "time"
+
+type Campaign struct {
+	CampaignId            *uint64                  `json:"campaignId"`
+	CoverImage            *string                  `json:"cover_image"`
+	Topic                 *string                  `json:"topic"`
+	Location              *string                  `json:"location"`
+	SchoolName            *string                  `json:"school_name"`
+	Description           *string                  `json:"description"`
+	IsCompleted           *bool                    `json:"is_completed"`
+	TelContact            *string                  `json:"tel_contact"`
+	CompletedAmount       *int                     `json:"completed_amount"`
+	CreatedAt             *time.Time               `json:"created_at" `
+	WantLists             []string             		`json:"want_lists" `
+	CampaignImages        []string        			`json:"campaign_images"`
+}
