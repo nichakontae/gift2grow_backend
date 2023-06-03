e6bc80ce51201a9cf88103b74d71467c906c7a29
Ryyyyyy12 <kevalin.sris@kmutt.ac.th>
2023-06-03 22:35:11 +0700

    update noti model
    - added isRead to user-noti
    - removed isRead from noti-object

--- loaders/mysql/model/noti_objects.go
+++ loaders/mysql/model/noti_objects.go
@@ -4,7 +4,6 @@ import "time"
 
 type NotiObject struct {
 	Id         *uint64     `json:"id" gorm:"primaryKey;not null"`
-	IsRead     *bool       `json:"is_read" gorm:"not null"`
 	CreatedAt  *time.Time  `json:"created_at" gorm:"not null"`
 	CampaignId *uint64     `json:"campaign_id" gorm:"primaryKey;not null"`
 	Campaign   *Campaign   `json:"campaign" gorm:"foreignKey:CampaignId;referencesId;not null"`
