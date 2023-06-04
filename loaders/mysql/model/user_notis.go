8e63303ca5d35cf87bf36f29b7f124c1c0321a76
Ryyyyyy12 <kevalin.sris@kmutt.ac.th>
2023-06-04 23:09:40 +0700

    add FK noti

--- loaders/mysql/model/user_notis.go
+++ loaders/mysql/model/user_notis.go
@@ -4,6 +4,6 @@ type UserNoti struct {
 	NotiObjectId *uint64     `json:"noti_object_id" gorm:"primaryKey;not null"`
 	NotiObject   *NotiObject `json:"noti_object" gorm:"foreignKey:NotiObjectId;references:Id;not null"`
 	UserId       *string     `json:"user_id" gorm:"primaryKey;not null"`
-	User         *User       `json:"user" gorm:"foreignKey:UserId;not null"`
+	User         *User       `json:"user" gorm:"foreignKey:UserId;references:Id;not null"`
 	IsRead       *bool       `json:"is_read" gorm:"not null"`
 }
