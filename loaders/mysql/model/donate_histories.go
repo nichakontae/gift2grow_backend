394388a561be19cdef30d810c9bed8db539eedde
Ryyyyyy12 <kevalin.sris@kmutt.ac.th>
2023-06-05 00:54:06 +0700

    add PK to tracking

--- loaders/mysql/model/donate_histories.go
+++ loaders/mysql/model/donate_histories.go
@@ -3,10 +3,10 @@ package model
 import "time"
 
 type DonateHistory struct {
+	TrackingNumber *string    `json:"tracking_number" gorm:"primaryKey;not null"`
 	CampaignId     *uint64    `json:"campaign_id" gorm:"primaryKey;not null"`
 	Campaign       *Campaign  `json:"campaign" gorm:"foreignKey:CampaignId;references:Id;not null"`
 	UserId         *string    `json:"user_id" gorm:"primaryKey;not null"`
 	User           *User      `json:"user" gorm:"foreignKey:UserId;references:Id;not null"`
-	TrackingNumber *string    `json:"tracking_number" gorm:"not null"`
 	DonationDate   *time.Time `json:"donation_date" gorm:"not null"`
 }
